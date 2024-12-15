afeaf22f1ffc170c33e733ddd9083e176981e033
Harindu Nimshan <harindumedawatta@gmail.com>
2024-12-15 14:37:26 +0530

    updated the server file

--- backend/server.js
+++ backend/server.js
@@ -4,10 +4,11 @@ import { connectDB } from './config/db.js';
 
 dotenv.config();
 const app = express();
+const port = process.env.PORT || 8080;
 
 app.get("/products", (req, res) => {});
 
-app.listen(5000, () => {
+app.listen(port, () => {
     connectDB();
-    console.log("Server started at http://localhost:5000");
+    console.log(`Server started at http://localhost:${port}`);
 });
